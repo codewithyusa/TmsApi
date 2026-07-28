d86a95eadcf0b7fef8e6ae8cccc49d2da6f8f240
Yusuf Umer <yusufumer26@gmail.com>
2026-07-28 15:48:20 +0300

    feat: add CourseDto field whitelist

--- TmsApi.Application/Dtos/CourseDto.cs
+++ TmsApi.Application/Dtos/CourseDto.cs
@@ -2,7 +2,21 @@ namespace TmsApi.Application.DTOs;
 
 public record CourseDto(
     int Id,
-    string Title,
     string Code,
+    string Title,
     int MaxCapacity,
     int EnrollmentCount);
+
+
+public static class CourseDtoFields
+{
+    public static readonly HashSet<string> Allowed =
+        new(StringComparer.OrdinalIgnoreCase)
+        {
+            nameof(CourseDto.Id),
+            nameof(CourseDto.Code),
+            nameof(CourseDto.Title),
+            nameof(CourseDto.MaxCapacity),
+            nameof(CourseDto.EnrollmentCount)
+        };
+}
